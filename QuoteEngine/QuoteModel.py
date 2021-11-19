499fe6eb80bf8fc9b7b73d95a7f37e7036992c67
Heather Mohorn <heather.mohorn@gmail.com>
2021-11-19 11:56:55 -0500

    Added ingestors for txt and csv

--- QuoteEngine/QuoteModel.py
+++ QuoteEngine/QuoteModel.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import csv
 
 class QuoteModel():
     def __init__(self, body, author):
@@ -16,3 +17,31 @@ class IngestorInterface(ABC):
     @abstractmethod
     def parse(cls, path: str) -> List[QuoteModel]
         pass
+
+class CSVIngestor(IngestorInterface):
+    def parse(cls, path):
+        quotes = []
+        with open(path, 'r') as infile:
+            reader = csv.DictReader(infile)
+            for elem in reader:
+                quote = QuoteModel(elem['body'], elem['author'])
+                quotes.append(quote)
+        return quotes
+
+class DOCXIngestor(IngestorInterface):
+    pass
+
+class PDFIngestor(IngestorInterface):
+    pass
+
+class TXTIngestor(IngestorInterface):
+    def parse(cls, path):
+        quotes = []
+        with open(path, 'r') as infile:
+            contents = infile.read()
+            lines = contents.split('\n')
+        for line in lines:
+            elements = line.split(' - ')
+            quote = QuoteModel(elements[0], elements[1])
+            quotes.append(quote)
+        return quotes
